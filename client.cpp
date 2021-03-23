bd1973af2b9cdccef3fbfe4a2e86b5ad6a98c1b6
Felicia1994 <jingma@bu.edu>
2021-03-23 13:51:19 -0400

    add comment b

--- client.cpp
+++ client.cpp
@@ -57,6 +57,7 @@ void my_send(int connfd, char *msg)
 int main(int argc, char **argv)
 {
     // a
+    // b
     // AF_INET: IPv4 protocols
     // SOCK_STREAM: stream socket
     // 0: default specific protocol
