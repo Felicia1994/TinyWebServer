a1b1333fe1bc2fac4d927aa7f8de921f0c98a372
Felicia1994 <jingma@bu.edu>
2021-04-01 17:31:28 -0400

    modified client.cpp into my style, and with better output messages

--- client.cpp
+++ client.cpp
@@ -10,52 +10,44 @@
 #include <arpa/inet.h> 
 using namespace std;
 
-//const
+// const
 const int BUFSIZE = 1024;
-const int LISTENQ = 5;
-const int PORT = 12345;
-//var
+// var
 sockaddr_in seraddr;
 pid_t childpid;
 char recvbuf[BUFSIZE];
 char sendbuf[BUFSIZE];
-//func
-void my_send(int connfd, char *msg)
+// func
+void my_send(int connfd)
 {
     while (1)
     {
-        //cout << msg << endl;
-        if (*msg == '\0')
-            cin >> sendbuf;
-        else
-            strcpy(sendbuf, msg);
-        
-        cout << "message created!" << endl;
+        cout << "waiting for message! input \"exit\" to exit!" << endl;
+        cin >> sendbuf;
+        cout << "message ok! message: " << sendbuf << endl;
 
-        //cin >> sendbuf;
-        bool ans = (*sendbuf);
-        //sleep(1);
-
-        int tmp = send(connfd, sendbuf, BUFSIZE, 0);
-        cout << "tmp: " << tmp << endl;
-        if (tmp <= 0 && ans != 0) {
-            cout << "client recv error" << endl;
+        if (strncmp(sendbuf,"exit",2)==0) {
+            cout << "send ends!" << endl;
             break;
-        } else if (ans == 0) {
-            cout << "end the send" << endl;
+        }
+
+        int send_ = send(connfd, sendbuf, BUFSIZE, 0);
+        if (send_==-1) {
+            cout << "send error!" << endl;
+            cout << strerror(errno) << endl;
             break;
         } else {
-            cout << "none of above!" << endl;
+            cout << "send ok! message: " << sendbuf << endl;
         }
-        cout << "here!" << endl;
-        sendbuf[0] = '\0'; //clear the buf
-        if (recv(connfd, recvbuf, BUFSIZE, 0) <= 0) {
-            cout << "client recv error" << endl;
-            break;
-        }
-        cout << "here!" << endl;
 
-        cout << recvbuf << endl;
+        int recv_ = recv(connfd, recvbuf, BUFSIZE, 0);
+        if (recv_==-1) {
+            cout << "receive error!" << endl;
+            cout << strerror(errno) << endl;
+            break;
+        } else {
+            cout << "receive ok! receive: " << recvbuf << endl;
+        }
     }
 }
 int main(int argc, char **argv)
@@ -65,33 +57,40 @@ int main(int argc, char **argv)
     // 0: default specific protocol
     // return int: non-neg if OK, or -1 if error
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    cout << sockfd << endl;    
-    cout << "socket ok!" << endl;
+    if (sockfd==-1) {
+        cout << "socket error!" << endl;
+        return -1;
+    } else {
+        cout << "socket ok! socket number: " << sockfd << endl;
+    }
 
     bzero(&seraddr, sizeof(seraddr));
     seraddr.sin_family = AF_INET;
     // convert IPv4 and IPv6 addresses from text to binary form
-    inet_pton(AF_INET,"127.0.0.1",&seraddr.sin_addr);
+    const char* ipaddr = new char[32];
+    if (argc>=2) ipaddr = argv[1];
+    else ipaddr = "127.0.0.1";
+    inet_pton(AF_INET,ipaddr,&seraddr.sin_addr);
     // htons converts the unsigned short integer hostshort from host byte order to network byte order
-    seraddr.sin_port = htons(PORT);
-    cout << "address ok!" << endl;
+    int portaddr = 12345;
+    if (argc>=3) portaddr = stoi(argv[2]);
+    seraddr.sin_port = htons(portaddr);
+    cout << "address ok! address: " << ipaddr << " port: " << portaddr << endl;
 
-    int conn = connect(sockfd, (const struct sockaddr *)&seraddr, sizeof(seraddr));
-    cout << conn << endl;
-    cout << strerror(errno) << endl;
-    cout << "connect ok!" << endl;
-
-    
-    if (argc == 2)
-        my_send(sockfd, argv[1]);
-    else {
-        char t = '\0';
-        my_send(sockfd,&t);
+    int conn_ = connect(sockfd, (const struct sockaddr *)&seraddr, sizeof(seraddr));
+    if (conn_==-1) {
+        cout << "connect error!" << endl;
+        cout << strerror(errno) << endl;
+        close(sockfd);
+        return -1;
+    } else {
+        cout << "connect ok!" << endl;
     }
-    cout << "send ok!" << endl;
-
+    
+    my_send(sockfd);
+    cout << "all send ok!" << endl;
     close(sockfd);
-    cout << "close connect" << endl;
+    cout << "connect close!" << endl;
     exit(0);
     return 0;
 }
